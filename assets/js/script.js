7963d513bf8d75aacdd8f3395c196d2777ce61b3
Lukas Keel <lrk83@cornell.edu>
2021-06-13 11:29:26 -0500

    Added some comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ var currentDay = document.querySelector("#currentDay");
 var rightNow = moment();
 var tasks=[];
 
-
 //Assign current day to the page
 currentDay.textContent=rightNow.format("dddd, MMMM Do YYYY");
 
+//Compare each time block to the current moment, and assign the right color
 var checkTimes = function(){
     for(x=0;x<9;x++){
         if (moment(tasks[x].time).diff(moment())>0){
@@ -20,14 +20,18 @@ var checkTimes = function(){
     };
 };
 
+//Load the tasks to do text from lcoal storage
 var loadTaskstoDo = function(){
     for (x=1;x<=9;x++){
         document.querySelector("#block"+x).innerHTML="<p>"+tasks[x-1].tasksToDo+"<p>";
     };
 }
 
+//Load any tasks saved in local storage
 var loadTasks = function(){
     tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    //If there are no tasks saved, make a new array to track tasks
     if (tasks===null){
         tasks=[];
         for(x=1;x<=9;x++){
@@ -40,6 +44,7 @@ var loadTasks = function(){
             tasks.push(taskDataObj);
         };
     }else{
+        //If there are tasks saved, update their times
         for(x=1;x<=9;x++){
             tasks[x-1].time=rightNow.format("MM.DD.YYYY")+" "+moment(document.querySelector("#time"+x).textContent, ["h:mm A"]).format("HH:mm")
         };
@@ -49,7 +54,7 @@ var loadTasks = function(){
 };
 
 
-
+//When time-blocks are clicked on make them into a textarea
 $(".time-block").on("click", "p", function() {
     // get current text of p element
     var text = $(this)
@@ -64,7 +69,7 @@ $(".time-block").on("click", "p", function() {
     textInput.trigger("focus");
 });
   
-// editable field was un-focused
+//When textareas are clicked away from, keep track of text the user has entered
 $(".time-block").on("blur", "textarea", function() {
     // get current value of textarea
     var text = $(this).val();
@@ -84,7 +89,11 @@ $(".time-block").on("blur", "textarea", function() {
     $(this).replaceWith(taskP);
 });
 
+
+//When save button is clicked save the text of the corresponding time-block
 $(".saveBtn").on("click", function(){
+
+    //Load a local tasks object so that I can save the text of this specific time block
     var tasksForSaving=JSON.parse(localStorage.getItem("tasks"));
     if (tasksForSaving===null){
         tasksForSaving=[];
