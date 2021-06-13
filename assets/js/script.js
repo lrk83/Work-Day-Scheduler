7cfd90579e1dfe0cf820b5912093890097234ff9
Lukas Keel <lrk83@cornell.edu>
2021-06-13 11:21:23 -0500

    loads tasks from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var currentDay = document.querySelector("#currentDay");
 var rightNow = moment();
 var tasks=[];
 
+
 //Assign current day to the page
 currentDay.textContent=rightNow.format("dddd, MMMM Do YYYY");
 
@@ -9,34 +10,45 @@ var checkTimes = function(){
     for(x=0;x<9;x++){
         if (moment(tasks[x].time).diff(moment())>0){
             if (moment(tasks[x].time).diff(moment(),"minutes")<60){
-                console.log(tasks[x].name);
                 var taskEl=document.getElementById(tasks[x].name)
                 taskEl.setAttribute("class","col-8 time-block present");
             }else{
                 var taskEl=document.getElementById(tasks[x].name);
-                taskEl.setAttribute("class","col-8 time-block present");
+                taskEl.setAttribute("class","col-8 time-block future");
             };
-        }
+        };
+    };
+};
+
+var loadTaskstoDo = function(){
+    for (x=1;x<=9;x++){
+        document.querySelector("#block"+x).innerHTML="<p>"+tasks[x-1].tasksToDo+"<p>";
     };
 }
 
 var loadTasks = function(){
     tasks = JSON.parse(localStorage.getItem("tasks"));
-    console.log(tasks);
     if (tasks===null){
         tasks=[];
         for(x=1;x<=9;x++){
             var taskDataObj = {
                 name: "block"+x,
-                location: document.querySelector("#block"+x),
                 time: rightNow.format("MM.DD.YYYY")+" "+moment(document.querySelector("#time"+x).textContent, ["h:mm A"]).format("HH:mm"),
-                tasksToDo: ""
+                tasksToDo: "",
+                btn: "btn"+x,
             };
             tasks.push(taskDataObj);
         };
+    }else{
+        for(x=1;x<=9;x++){
+            tasks[x-1].time=rightNow.format("MM.DD.YYYY")+" "+moment(document.querySelector("#time"+x).textContent, ["h:mm A"]).format("HH:mm")
+        };
     };
     checkTimes();
-}
+    loadTaskstoDo();
+};
+
+
 
 $(".time-block").on("click", "p", function() {
     // get current text of p element
@@ -61,11 +73,11 @@ $(".time-block").on("blur", "textarea", function() {
     var taskP = $("<p>")
         .text(text);
 
-    //save next text to the tasks array
-    for(x=0;x<9;x++){
-        if (tasks[x].location.id===$(this).closest(".time-block")[0].id){
+    //save text to the tasks array
+    for (x=0;x<9;x++){
+        if (tasks[x].name===$(this).closest(".time-block")[0].id){
             tasks[x].tasksToDo=text;
-        }
+        };
     };
 
     // replace textarea with new content
@@ -73,9 +85,27 @@ $(".time-block").on("blur", "textarea", function() {
 });
 
 $(".saveBtn").on("click", function(){
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    console.log(JSON.parse(localStorage.getItem("tasks")));
-    console.log(JSON.stringify(tasks));
+    var tasksForSaving=JSON.parse(localStorage.getItem("tasks"));
+    if (tasksForSaving===null){
+        tasksForSaving=[];
+        for(x=1;x<=9;x++){
+            var taskDataObj = {
+                name: "block"+x,
+                time: rightNow.format("MM.DD.YYYY")+" "+moment(document.querySelector("#time"+x).textContent, ["h:mm A"]).format("HH:mm"),
+                tasksToDo: "",
+                btn: "btn"+x,
+            };
+            tasksForSaving.push(taskDataObj);
+        };
+    };
+    for (x=0;x<9;x++){
+        console.log($(this)[0].id);
+        console.log(tasks[x].btn);
+        if (tasks[x].btn===$(this)[0].id){
+            tasksForSaving[x].tasksToDo=tasks[x].tasksToDo;
+        };
+    };
+    localStorage.setItem("tasks", JSON.stringify(tasksForSaving));
 });
 
 loadTasks();
